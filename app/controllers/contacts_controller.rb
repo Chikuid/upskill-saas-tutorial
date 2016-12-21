e03471a4c2be33a8a1b8b9b1f903140a488d79e4
Shivam Singh <chikuid119@gmail.com>
2016-12-21 16:23:17 +0000

    Add code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,12 @@
 class ContactsController < ApplicationController
+  # GET request to /contact-us
   def new
     @contact = Contact.new
   end
   
+  # POST request to /contacts
   def create
+    # Mass assignment of contact form fields to contact object
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Message sent."
@@ -13,12 +16,14 @@ class ContactsController < ApplicationController
       ContactMailer.contact_email(name, email, body).deliver
       redirect_to new_contact_path
     else
+      # Flash message with errors in case @contact doesn't save
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
   end
   
   private
+    # Whitelisting contact form parameters.
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
